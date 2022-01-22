915be4ff4dc8240f79f131c5b2e94f1279830f40
Anthony Sanchez <sanchezac@google.com>
2022-01-22 00:05:38 -0700

    removed attempt at moving files, will instruct to do manually.

--- deploy_dekart_appengine.sh
+++ deploy_dekart_appengine.sh
@@ -13,18 +13,9 @@ gcloud auth login ${USER_EMAIL}
 echo "Set default project"
 gcloud config set project ${PROJECT_ID}
 
-#pull LATEST dekart build from git repo
-#update to reflect version you want
-echo "Pulling dekart repo from public git repo"
-echo "Newer versions may require alteration beyond what this script supports"
-echo "At the time of development the latest version was v0.7.1"
-echo "https://github.com/dekart-xyz/dekart/releases/tag/v0.7.1"
-echo "https://github.com/dekart-xyz/dekart/tree/v0.7.1"
-
-git clone https://github.com/dekart-xyz/dekart.git
-
-# change directories to dekart app engine deployment
-cd dekart/install/app-engine
+echo "Deploying dekart application on App Engine"
+echo "This can take several minutes"
+gcloud app deploy dekart.yaml --quiet
 
 
 
