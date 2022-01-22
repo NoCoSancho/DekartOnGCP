b2df340c01a57ba11e1ef31bb12097921b9aafcf
Anthony Sanchez <sanchezac@google.com>
2022-01-21 22:37:55 -0700

    removed iam

---
+++ setup_iam.sh
@@ -0,0 +1,27 @@
+##################################################
+##
+## Set required IAM permissions
+##
+##################################################
+
+# source the previously set env variables
+source ./config.sh
+
+# prompt user to login
+gcloud auth login ${USER_EMAIL}
+echo "Assigning IAM Permissions"
+
+#get project number via project id to identify cloudbuild service account name
+PROJECT_NUM=`gcloud projects list \
+    --filter="$(gcloud config get-value project)" \
+    --format="value(PROJECT_NUMBER)"`
+
+#default cloud build account
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member=serviceAccount:${PROJECT_NUM}@cloudbuild.gserviceaccount.com \
+    --role=roles/cloudbuild.builds.builder
+
+#default app engine service account
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member=serviceAccount:${PROJECT_ID}@appspot.gserviceaccount.com \
+    --role=roles/editor
