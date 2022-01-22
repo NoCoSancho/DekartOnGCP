d0fc98e8d459c519d2773c900f7f10494f517ecd
Anthony Sanchez <sanchezac@google.com>
2022-01-21 23:02:36 -0700

    set default project

--- setup_appengine.sh
+++ setup_appengine.sh
@@ -10,6 +10,9 @@ source ./config.sh
 # prompt user to login
 gcloud auth login ${USER_EMAIL}
 
+echo "Set default project"
+gcloud config set project ${PROJECT_ID}
+
 echo "Creating App Engine app" 
 
 gcloud app create --region=${APP_ENGINE_REGION}
