bb14eaed62eceff415b826d484d85d4a3371bfcb
Anthony Sanchez <sanchezac@google.com>
2022-01-21 23:01:47 -0700

    set default project up front

--- setup_cloudstorage.sh
+++ setup_cloudstorage.sh
@@ -10,6 +10,9 @@ source ./config.sh
 # prompt user to login
 gcloud auth login ${USER_EMAIL}
 
+echo "Set default project"
+gcloud config set project ${PROJECT_ID}
+
 echo "Creating unique dekart GCS Bucket" 
 
 gsutil mb -b on -l ${REGION} gs://${PROJECT_ID}-dekart
